6ebbe50cc2c2c9a13fcbbf138d28f0976ac922dd
pigfyy <111782362+pigfyy@users.noreply.github.com>
2022-09-23 12:39:47 -0700

    added edit and delete buttons, also made delete button work

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ function collapseEls() {
           completedEl.classList.add("active");
           completedSelectorEl.innerText = "remove";
         }
-      } else {
+      } else if (selector.innerText == "remove") {
         if (selector.id === "incomplete-selector") {
           incompleteEl.classList.remove("active");
           incompleteSelectorEl.innerText = "add";
@@ -46,44 +46,67 @@ function onTaskEnter() {
 
 // adds task to one of the lists, params: task name, isToDo or isCompleted
 function addTask(name, isToDo) {
-  const list = document.createElement("li");
   const span = document.createElement("span");
-  const div = document.createElement("div");
 
-  list.classList.add("list-el");
   span.classList.add("in-list", "material-symbols-outlined");
-  div.innerText = name;
-
-  if (isToDo) {
+  if (isToDo === true) {
     span.classList.add("not-done");
+  }
+  if (isToDo === true) {
     span.innerText = "radio_button_unchecked";
-    div.classList.add("list-el-text");
-    list.appendChild(span);
-    list.appendChild(div);
-    incompleteEl.appendChild(list);
-    span.addEventListener("click", (e) => {
-      const parent = span.parentElement;
-      const taskName = span.nextElementSibling.innerText;
-      parent.remove();
-      addTask(taskName, false);
-    });
   } else {
     span.innerText = "radio_button_checked";
-    div.classList.add("list-el-text", "completed-task");
-    list.appendChild(span);
-    list.appendChild(div);
+  }
+  const div = document.createElement("div");
+  div.classList.add("list-el-text");
+  if (isToDo === false) {
+    div.classList.add("completed-task");
+  }
+  div.innerText = name;
+
+  const row = document.createElement("div");
+  row.classList.add("row");
+  row.appendChild(span);
+  row.appendChild(div);
+
+  const span2 = document.createElement("span");
+  span2.classList.add("selector", "edit", "hover", "material-symbols-outlined");
+  span2.innerText = "edit";
+  const span3 = document.createElement("span");
+  span3.classList.add(
+    "selector",
+    "close",
+    "hover",
+    "material-symbols-outlined"
+  );
+  span3.innerText = "close";
+
+  const div2 = document.createElement("div");
+  div2.appendChild(span2);
+  div2.appendChild(span3);
+
+  const list = document.createElement("li");
+  list.classList.add("list-el");
+  list.appendChild(row);
+  list.appendChild(div2);
+
+  if (isToDo === true) {
+    incompleteEl.appendChild(list);
+  } else {
     completedEl.appendChild(list);
   }
-}
 
-addListenerToSpans();
-function addListenerToSpans() {
-  spanEls.forEach((span) => {
-    span.addEventListener("click", (e) => {
-      const parent = span.parentElement;
-      const taskName = span.nextElementSibling.innerText;
-      parent.remove();
-      addTask(taskName, false);
-    });
+  span.addEventListener("click", () => {
+    const parent = span.parentElement.parentElement;
+    const taskName = span.nextElementSibling.innerText;
+    parent.remove();
+    console.log("HII");
+    addTask(taskName, false);
+  });
+  span2.addEventListener("click", () => {
+    alert("feature isn't ready yet!");
+  });
+  span3.addEventListener("click", () => {
+    span3.parentElement.parentElement.remove();
   });
 }
